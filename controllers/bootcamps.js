ac71a637eaa4613b8c43eee392c782a1d91346d5
unknownpp12 <amanpokharel51@gmail.com>
2025-06-29 20:46:03 +0545

    Create bootcamp POST added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 //@desc     Get all bootcamps
 //@route    GET api/v1/bootcamps
 //@access   Public 
@@ -15,9 +17,19 @@ exports.getBootcamp = (req,res,next) =>{
 //@desc     Create new bootcamp
 //@route    POST api/v1/bootcamps
 //@access   Private
-exports.createBootcamps = (req,res,next) =>{
-    res.status(200).json({success:true,message:'Create all bootcamps'});
-} 
+exports.createBootcamps = async (req,res,next) =>{
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+
+        res.status(201).json({
+        success:true,
+        data:bootcamp
+    });
+        
+    } catch (error) {
+        res.status(400).json({success:false});
+    }
+};
 
 //@desc     Update bootcamp
 //@route    PUT api/v1/bootcamps/:id
